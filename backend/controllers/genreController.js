a9a8d8e067c836372a069ea02df9b48759471d82
Malidu Pahasara <malidupahasara04@gmail.com>
2025-02-13 00:42:40 +0530

    frontend changes

--- backend/controllers/genreController.js
+++ backend/controllers/genreController.js
@@ -1,79 +1,83 @@
-import Genre from "../models/Genre";
-import asyncHandler from '../middlewares/asyncHandler';
+import Genre from "../models/Genre.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
 
 const createGenre = asyncHandler(async (req, res) => {
-    try {
-        const { name } = req.body;
-        if (!name) {
-            return res.json({ error: 'Name is required' });
-        }
+  try {
+    const { name } = req.body;
 
-        const existingGenre = await Genre.findOne({ name });
-        if (existingGenre) {
-            return res.json({ error: 'Already exists' });
-        }
-
-        const genre = await new Genre({ name }).save();
-        res.json(genre);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json(error);
+    if (!name) {
+      return res.json({ error: "Name is required" });
     }
+
+    const existingGenre = await Genre.findOne({ name });
+
+    if (existingGenre) {
+      return res.json({ error: "Already exists" });
+    }
+
+    const genre = await new Genre({ name }).save();
+    res.json(genre);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
 });
 
 const updateGenre = asyncHandler(async (req, res) => {
-    try {
-        const { name } = req.body;
-        const { id } = req.params;
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
 
-        const genre = await Genre.findOne({ _id: id });
-        if (!genre) {
-            return res.status(404).json({ error: 'Genre not found' });
-        }
+    const genre = await Genre.findOne({ _id: id });
 
-        genre.name = name;
-        const updateGenre = await genre.save();
-        res.json(updateGenre);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!genre) {
+      return res.status(404).json({ error: "Genre not found" });
     }
+
+    genre.name = name;
+
+    const updatedGenre = await genre.save();
+    res.json(updatedGenre);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 const removeGenre = asyncHandler(async (req, res) => {
-    try {
-        const { id } = req.params;
-        const removed = await Genre.findByIdAndDelete(id);
+  try {
+    const { id } = req.params;
+    const removed = await Genre.findByIdAndDelete(id);
 
-        if (!removed) {
-            return res.status(404).json({ error: 'Genre not found' });
-        }
-
-        res.json(removed);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!removed) {
+      return res.status(404).json({ error: "Genre not found" });
     }
+
+    res.json(removed);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Interval server error" });
+  }
 });
 
 const listGenres = asyncHandler(async (req, res) => {
-    try {
-        const all = await Genre.find({});
-        res.json(all);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json(error.message);
-    }
+  try {
+    const all = await Genre.find({});
+    res.json(all);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
 });
 
 const readGenre = asyncHandler(async (req, res) => {
-    try {
-        const genre = await Genre.findOne({ _id: req.params.id });
-        res.json(genre);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json(error.message);
-    }
+  try {
+    const genre = await Genre.findOne({ _id: req.params.id });
+    res.json(genre);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
 });
 
 export { createGenre, updateGenre, removeGenre, listGenres, readGenre };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,127 +1,128 @@
-import User from '../models/User.js';
-import bcrypt from 'bcryptjs';
-import asyncHandler from '../middleware/asyncHandler';
-import createToken from '../utils/createToken';
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
 
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-        throw new Error('Please fill the blanks');
-    }
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the fields");
+  }
 
-    const userExists = await User.findOne({ email });
-    if (userExists)
-        res.status(400).send("User already registered");
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("User already exists");
 
-    const salt = await bcrpyt.genSalt(10);
-    const hashedPassword = await bcrpyt.hash(password, salt);
-    const newUser = new User({ username, email, password: hashedPassword });
+  // Hash the user password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
 
-    try {
-        await newUser.save();
-        createToken(res, newUser._id);
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
 
-        res.status(201).json({
-            _id: newUser._id,
-            username: newUser.username,
-            email: newUser.email,
-            idAdmin: newUser.isAdmin,
-        });
-    } catch (error) {
-        res.status(400);
-        throw new Error("Invalid User Data");
-    }
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+  const existingUser = await User.findOne({ email });
 
-    if (existingUser) {
-        const isPasswordValid = await bcrpyt.compare(
-            password,
-            existingUser.password
-        );
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
 
-        if (isPasswordValid) {
-            createToken(res, existingUser._id);
-            res.status(201).json({
-                _id: existingUser._id,
-                username: existingUser.username,
-                email: existingUser.email,
-                isAdmin: existingUser.isAdmin,
-            });
-        } else {
-            res.status(401).json({ message: 'Invalid Password' });
-        }
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
     } else {
-        res.status(401).json({ message: 'User not found' });
+      res.status(401).json({ message: "Invalid Password" });
     }
+  } else {
+    res.status(401).json({ message: "User not found" });
+  }
 });
 
 const logoutCurrentUser = asyncHandler(async (req, res) => {
-    res.cookie("jwt", "", {
-        httpOnly: true,
-        expiers: new Date(0),
-    });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
 
-    res.status(200).json({ message: 'Logged out successfully' });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
-const getAllUsers = asyncHandler(async(req, res) => {
-    const users = await User.find({});
-    res.json(users);
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
 });
 
 const getCurrentUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-    if(user) {
-        res.json({
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-        });
-    } else {
-        res.status(404);
-        throw new Error("user not found");
-    }
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
 });
 
-const updateCurrentUserProfile = asyncHandler(async(req, res) => {
-    const user = await User.findById(req.user._id);
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
-    if(user) {
-        user.username = req.body.username || user.username;
-        user.email = req.body.email || user.email;
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
 
-        if(req.body.password) {
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrpyt.hash(req.body.password, salt);
-            user.password = hashedPassword;
-        }
-
-        const updateUser = await user.save();
-
-        res.json({
-            _id: updateUser._id,
-            username: updateUser.username,
-            email: updateUser.email,
-            isAdmin: updateUser.isAdmin,
-        });
-    } else {
-        res.status(404);
-        throw new Error("User not found");
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
     }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
-    createUser,
-    loginUser,
-    logoutCurrentUser,
-    getAllUsers,
-    getCurrentUserProfile,
-    updateCurrentUserProfile
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
 };
