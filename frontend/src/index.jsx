aaa9ac0a560e7674cf3e6eb07c9a6728d026487b
Malidu Pahasara <malidupahasara04@gmail.com>
2025-02-14 00:47:06 +0530

    changes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,16 @@
 import store from './redux/store.js';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
+
+import Home from './pages/Home.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      
+      <Route index={true} path='/' element={<Home />} />
     </Route>
   )
 )
